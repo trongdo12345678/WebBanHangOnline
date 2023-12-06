0e4c7dae3570392bf1e48ddcc96f9df33b3ca270
Trọng Đỗ <nguyendotrong2003@gmail.com>
2023-12-06 22:28:49 +0700

    Finished category section

--- WebBanHangOnline/Areas/Admin/Controllers/CategoryController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/CategoryController.cs
@@ -18,6 +18,7 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
             var items = db.Categorles;
             return View(items);
         }
+        //add thêm sản phẩm
         public ActionResult Add()
         {
             return View();
@@ -37,6 +38,49 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
             }
             return View(model);
             
+        }
+        //update sản phẩm
+        public ActionResult Edit(int Id)
+        {
+            var item =db.Categorles.Find(Id);
+            return View(item);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categorys model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categorles.Attach(model);
+                model.ModifiedrDate = DateTime.Now;
+                model.Alias = WebBanHangOnline.Models.Commons.Filter.FilterChar(model.Title);
+                db.Entry(model).Property(x => x.Title).IsModified = true;
+                db.Entry(model).Property(x => x.Description).IsModified = true;
+                db.Entry(model).Property(x => x.Alias).IsModified = true;
+                db.Entry(model).Property(x => x.SeoDescripton).IsModified = true;
+                db.Entry(model).Property(x => x.SeoKeywords).IsModified = true;
+                db.Entry(model).Property(x => x.SeoTitle).IsModified = true;
+                db.Entry(model).Property(x => x.Position).IsModified = true;
+                db.Entry(model).Property(x => x.ModifiedrDate).IsModified = true;
+                db.Entry(model).Property(x => x.ModifierBy).IsModified = true;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var item = db.Categorles.Find(id);
+            if (item != null)
+            {
+                //var DeleteItem = db.Categorles.Attach(item);    
+                db.Categorles.Remove(item);
+                db.SaveChanges();
+                return Json(new {success = true });
+            }
+            return Json(new { success = false });
         }
     }
 }
