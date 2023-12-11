b9373349f971680c1376f23aa36ef3fc8b9cb1c9
Trọng Đỗ <nguyendotrong2003@gmail.com>
2023-12-11 22:20:37 +0700

    add products

---
+++ WebBanHangOnline/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHangOnline.Models;
+using WebBanHangOnline.Models.Entity;
+
+namespace WebBanHangOnline.Areas.Admin.Controllers
+{
+    
+    public class ProductsController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Admin/Product
+        public ActionResult Index(int? page)
+        {
+           
+            IEnumerable<Products> items = db.Products.OrderByDescending(x=> x.Id);
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
