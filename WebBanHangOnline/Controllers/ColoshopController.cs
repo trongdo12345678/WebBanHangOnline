02a85c51b3bdc4380c08723025e72b5d5ca38163
Trọng Đỗ <nguyendotrong2003@gmail.com>
2024-02-21 21:06:42 +0700

    update

---
+++ WebBanHangOnline/Controllers/ColoshopController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHangOnline.Controllers
+{
+    public class ColoshopController : Controller
+    {
+        // GET: Coloshop
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Categories()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Single()
+        {
+            return View();
+        }
+    }
+}
